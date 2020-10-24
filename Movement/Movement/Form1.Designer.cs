662a8b7d4693eaa47135e32ddb71e1180205dcde
MrWittich <54331644+MrWittich@users.noreply.github.com>
2020-10-24 11:33:02 -0700

    Add files via upload

---
+++ Movement/Movement/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Movement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picHeart = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picHeart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picHeart
+            // 
+            this.picHeart.Image = ((System.Drawing.Image)(resources.GetObject("picHeart.Image")));
+            this.picHeart.InitialImage = ((System.Drawing.Image)(resources.GetObject("picHeart.InitialImage")));
+            this.picHeart.Location = new System.Drawing.Point(12, 388);
+            this.picHeart.Name = "picHeart";
+            this.picHeart.Size = new System.Drawing.Size(50, 50);
+            this.picHeart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picHeart.TabIndex = 0;
+            this.picHeart.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picHeart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.picHeart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picHeart;
+    }
+}
+
---
+++ Movement/Movement/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Movement
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'w')
+            {
+                picHeart.Top -= 10;
+
+            }
+            else if (e.KeyChar == 's')
+            {
+                picHeart.Top += 10;
+            }
+            else if (e.KeyChar == 'a')
+            {
+                picHeart.Left -= 10;
+            }
+            else if (e.KeyChar == 'd')
+            {
+                picHeart.Left += 10;
+            }
+            else if (e.KeyChar == 'p')
+            {
+                Size icon = picHeart.Size;
+                icon.Height -= 10;
+                icon.Width -= 10;
+                picHeart.Size = icon;
+            }
+        }
+    }
+}
